ff3a5671037224a16db8b5a2982a2781c3c05771
Pol Vallverdu <pvallverdualcaide@gmail.com>
2022-07-07 16:09:57 +0200

    Fixed apaño

--- lib/screens/PasswordScreen.dart
+++ lib/screens/PasswordScreen.dart
@@ -25,54 +25,45 @@ class PasswordScreen extends HookWidget {
     }
 
     return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
-
-          if (!currentFocus.hasPrimaryFocus) {
-            currentFocus.unfocus();
-          }
-        },
-        child: Container(
-          height: double.infinity,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Password",
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Container(
-                width: width * 0.5,
-                child: TextField(
-                  obscureText: true,
-                  controller: _passController,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                  ),
-                  onSubmitted: (_) => _login(remember.value),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Password",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Container(
+              width: width * 0.5,
+              child: TextField(
+                obscureText: true,
+                controller: _passController,
+                decoration: InputDecoration(
+                  labelText: "Password",
                 ),
+                onSubmitted: (_) => _login(remember.value),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Checkbox(
-                      value: remember.value,
-                      onChanged: (newValue) {
-                        final val = newValue ?? false;
-                        remember.value = val;
-                      }),
-                  Text("Recordar password"),
-                ],
-              ),
-              TextButton(
-                child: Text("Login"),
-                onPressed: () => _login(remember.value),
-              ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                    value: remember.value,
+                    onChanged: (newValue) {
+                      final val = newValue ?? false;
+                      remember.value = val;
+                    }),
+                Text("Recordar password"),
+              ],
+            ),
+            TextButton(
+              child: Text("Login"),
+              onPressed: () => _login(remember.value),
+            ),
+          ],
         ),
       ),
     );
